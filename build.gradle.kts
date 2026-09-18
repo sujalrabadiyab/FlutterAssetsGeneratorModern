95a4ed376e08d4408f0c974447e76f36d760f841
Sujal Rabadiya <sujalrabadiya.beetonz@gmail.com>
2026-09-18 18:29:36 +0530

    Initial Signed  Release

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,6 +66,16 @@ intellijPlatform {
             recommended()
         }
     }
+
+    signing {
+        certificateChainFile.set(file("certificate/chain.crt"))
+        privateKeyFile.set(file("certificate/private.pem"))
+        password.set(providers.environmentVariable("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishing {
+        token.set(providers.environmentVariable("PUBLISH_TOKEN"))
+    }
 }
 
 tasks {
